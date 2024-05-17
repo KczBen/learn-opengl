e761b3d342f19417d8608421a9defac7c867608a
Bence Koczogh <bek30@hi.is>
2024-05-17 22:22:43 +0000

    Properly set gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bence/Documents/CodingProjects/C++/opengl
-# Build directory: /home/bence/Documents/CodingProjects/C++/opengl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
