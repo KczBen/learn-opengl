b11b167edceda3fa14dd877db179947ee72bbeb5
Bence Koczogh <bek30@hi.is>
2024-05-24 19:05:34 +0000

    Remove unused vertex colour

--- main.cpp
+++ main.cpp
@@ -76,11 +76,6 @@ glm::vec3 cubePositions[] {
     glm::vec3(-1.3f,  1.0f, -1.5f),
 };
 
-unsigned int indices[] {
-    0, 1, 3,
-    1, 2, 3
-};
-
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -137,30 +132,21 @@ int main() {
     // Create vertex buffer, vertex array and element buffer
     unsigned int VBO;
     unsigned int VAO;
-    unsigned int EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     // Position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Colour
-    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
-    // glEnableVertexAttribArray(1);
-
     // Texture coordinates
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     Shader customShader("../shaders/vertex.shader", "../shaders/fragment.shader");
     customShader.use();
